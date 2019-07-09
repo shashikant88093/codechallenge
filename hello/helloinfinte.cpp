9b8c6fb2ccb38480150a4be76a0ffbce548ab242
shashikant88093 <42858165+shashikant88093@users.noreply.github.com>
2019-07-09 11:09:58 +0530

    Add files via upload

---
+++ hello/helloinfinte.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str;
+    int a;
+    cin >> a;
+    for (int i = 0; i < a; i++)
+    {
+        str = "hello";
+        cout << str<<endl;
+    }
+
+    return 0;
+}
