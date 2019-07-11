76d60287fa2b18dc47298ec493f93fe44f108019
shashikant88093 <42858165+shashikant88093@users.noreply.github.com>
2019-07-11 14:08:43 +0530

    Add files via upload

---
+++ thriteen/prime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, num,flag=0;
+    cin >> num;
+    for (i = 2; i <= num/2; i++)
+    {
+        if (num%i==0)
+        {
+            cout << "no";
+            flag=1;
+            break;
+        }
+        if(flag==0){
+            cout<<"yes";
+        }
+        
+    }
+
+    return 0;
+}
